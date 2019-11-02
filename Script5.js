0cc7f0c8921aa57619598d1a6e4354300fa4efd0
Carlos Coca <CACocagarciap@outlook.com>
2019-11-02 17:46:23 -0500

    Add files via upload
    The bootstrap files were on their own folder but I can't upload a folder. If you try to run the interface without the proper location of the files, several pages won't work.

---
+++ Script5.js
@@ -0,0 +1,28 @@
+/* DECLARING A CITY VARIABLE FOR THE URL */ 
+var city = "Nashville"; 
+
+/**** CALL IN THE JSON DATA FILE (API) TO YOUR WEBSITE */ /* ADDING A STATIC CITY VARIABLE WITHIN THE URL */ 
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&", function(data){
+    console.log(data);
+
+    /**************************************************************************************** VARIABLES */ 
+
+    /* ICON */
+    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    /* LETS CALL THE ICON AND SEE IF IT WORKS */
+   /*  console.log(icon); */ 
+
+   /* WEATHER */
+   var weather = data.weather[0].main;
+
+   /* TEMP */ /* Math.round ; round the value to the nearest number */ 
+   var temp = Math.round(data.main.temp);
+
+   /***************************************************************************************** FUNCTIONS */ 
+   $(".icon").attr("src", icon);
+   $(".weather").append(weather);
+   $(".temp").append(temp);
+   $(".city").append(city); 
+
+
+})
